62aa451a3fb1f8bca09acb65c534647cd4f00f1f
unknown <santiagobalino@hotmail.com>
2021-11-19 07:16:11 -0300

    I added the last parts of the page

--- src/Components/Page/Page.tsx
+++ src/Components/Page/Page.tsx
@@ -2,13 +2,31 @@ import React, { FunctionComponent } from "react";
 import GlobalStyle from "./styles/globalstyle";
 
 import {
+  BottomButtons,
+  BottomContainer,
+  BottomSubtitle,
+  BottomText,
+  ClipboardContainer,
+  ClipboardImage,
   ComputerImage,
   Container,
   ContainerImage,
   ContainerItems,
   ContainerMode,
+  ContainerTools,
+  ContainerWorkflow,
+  Footer,
+  FooterContainer,
+  FooterIconsContainer,
+  FooterLink,
+  FooterLinkImg,
+  FooterLinks,
+  FooterLogo,
+  FooterTextContainer,
   Header,
   IconContainer,
+  Icons,
+  IconsContainer,
   IconText,
   IconTheme,
   ImageItems,
@@ -19,6 +37,7 @@ import {
   Logo,
   LogoContainer,
   LogoImg,
+  MiniContainer,
   Navbar,
   NavMenu,
   NavMenuItem,
@@ -27,13 +46,32 @@ import {
   Subtitle,
   SubtitleContainer,
   SubtitleText,
+  SubtitleWorkflow,
+  TextImage,
+  TextTools,
+  TextWorkflow,
   Title,
+  TitleImage,
   TitleText,
+  TitleTools,
+  WorkflowImg,
 } from "./styles";
 
 import iconTheme from "./images/sun.svg";
 import logo from "./images/logo.jpg";
 import computerImage from "./images/image-computer.png";
+import clipboardImage from "./images/image-devices.png";
+import blacklistImage from "./images/icon-blacklist.svg";
+import iconTextImage from "./images/icon-text.svg";
+import iconPreviewImage from "./images/icon-preview.svg";
+import logoGoogle from "./images/logo-google.png";
+import logoIbm from "./images/logo-ibm.png";
+import logoMicrosoft from "./images/logo-microsoft.png";
+import logoHp from "./images/logo-hp.png";
+import logoVector from "./images/logo-vector-graphics.png";
+import iconFacebook from "./images/icon-facebook.svg";
+import iconTwitter from "./images/icon-twitter.svg";
+import iconInstagram from "./images/icon-instagram.svg";
 
 let url = "#";
 
@@ -115,6 +153,101 @@ export const Page: FunctionComponent = () => {
           </ImageText>
         </ImageItems>
       </ContainerImage>
+      <ClipboardContainer>
+        <TitleImage>Access Clipboard anywhere</TitleImage>
+        <TextImage>
+          Whether you’re on the go, or at your computer, you can access all your
+          Clipboard snippets in a few simple clicks.
+        </TextImage>
+        <ClipboardImage src={clipboardImage} />
+      </ClipboardContainer>
+      <ContainerTools>
+        <TitleTools>Supercharge your workflow</TitleTools>
+        <TextTools>We’ve got the tools to boost your productivity.</TextTools>
+        <ContainerWorkflow>
+          <MiniContainer>
+            <WorkflowImg src={blacklistImage} />
+            <SubtitleWorkflow>Create blacklists</SubtitleWorkflow>
+            <TextWorkflow>
+              Ensure sensitive information never makes its way to your clipboard
+              by excluding certain sources.
+            </TextWorkflow>
+          </MiniContainer>
+          <MiniContainer>
+            <WorkflowImg src={iconTextImage} />
+            <SubtitleWorkflow>Plain text snippets</SubtitleWorkflow>
+            <TextWorkflow>
+              Remove unwanted formatting from copied text for a consistent look.
+            </TextWorkflow>
+          </MiniContainer>
+          <MiniContainer>
+            <WorkflowImg src={iconPreviewImage} />
+            <SubtitleWorkflow>Sneak preview</SubtitleWorkflow>
+            <TextWorkflow>
+              Quick preview of all snippets on your Clipboard for easy access.
+            </TextWorkflow>
+          </MiniContainer>
+        </ContainerWorkflow>
+      </ContainerTools>
+      <IconsContainer>
+        <Icons src={logoGoogle} />
+        <Icons src={logoIbm} />
+        <Icons src={logoMicrosoft} />
+        <Icons src={logoHp} />
+        <Icons src={logoVector} />
+      </IconsContainer>
+      <BottomContainer>
+        <BottomSubtitle>Clipboard for iOS and Mac OS</BottomSubtitle>
+        <BottomText>
+          Available for free on the App Store. Download for Mac or iOS, sync
+          with iCloud and you’re ready to start adding to your clipboard.
+        </BottomText>
+        <BottomButtons>
+          <Paragraph>
+            <Link variant="ios">Download for iOS</Link>
+          </Paragraph>
+          <Paragraph>
+            <Link>Download for Mac</Link>
+          </Paragraph>
+        </BottomButtons>
+      </BottomContainer>
+      <Footer>
+        <FooterLogo src={logo} width="50px" height="50px" />
+        <FooterContainer>
+          <FooterTextContainer>
+            <Paragraph>
+              <FooterLinks>FAQs</FooterLinks>
+            </Paragraph>
+            <Paragraph>
+              <FooterLinks>Contact Us</FooterLinks>
+            </Paragraph>
+          </FooterTextContainer>
+          <FooterTextContainer>
+            <Paragraph>
+              <FooterLinks>Privacy Policy</FooterLinks>
+            </Paragraph>
+            <Paragraph>
+              <FooterLinks>Press Kit</FooterLinks>
+            </Paragraph>
+          </FooterTextContainer>
+          <FooterTextContainer>
+            <Paragraph>
+              <FooterLinks>Install Guide</FooterLinks>
+            </Paragraph>
+          </FooterTextContainer>
+        </FooterContainer>
+        <FooterIconsContainer>
+          <FooterLink href={url}>
+            <FooterLinkImg src={iconFacebook} />
+          </FooterLink>
+          <FooterLink href={url}>
+            <FooterLinkImg src={iconTwitter} />
+          </FooterLink>
+          <FooterLink href={url}>
+            <FooterLinkImg src={iconInstagram} />
+          </FooterLink>
+        </FooterIconsContainer>
+      </Footer>
     </>
   );
 };
--- src/Components/Page/styles/index.tsx
+++ src/Components/Page/styles/index.tsx
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
 
 // Header / navbar
 
@@ -74,6 +74,12 @@ export const NavMenuLink = styled.a`
   color: white;
   font-size: 20px;
   font-weight: 300;
+  &:hover {
+    background-color: #a71226;
+    padding: 8px 12px;
+    border-radius: 6px;
+    transition: 0.5s;
+  }
 `;
 
 // here starts the main section
@@ -188,4 +194,192 @@ export const ImageText = styled.p`
   margin-bottom: 70px;
   line-height: 1.5;
   width: 85%;
+`;
+
+// clipboard container
+
+export const ClipboardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 100px;
+`;
+
+export const TitleImage = styled.h2`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
+  font-size: 2.5em;
+`;
+
+export const TextImage = styled.p`
+  margin-left: 20%;
+  margin-right: 20%;
+  text-align: center;
+  color: hsl(201, 11%, 66%);
+  font-size: 0.9em;
+  margin-bottom: 50px;
+  font-size: 1.2em;
+`;
+
+export const ClipboardImage = styled.img`
+  height: 50vh;
+  margin: 0 auto;
+  width: 50%;
+`;
+
+// container tools
+
+export const ContainerTools = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const TitleTools = styled.h2`
+  text-align: center;
+  margin-bottom: 10px;
+  font-size: 2.5em;
+`;
+
+export const TextTools = styled.p`
+  text-align: center;
+  color: hsl(201, 11%, 66%);
+  font-size: 1.2em;
+`;
+
+export const ContainerWorkflow = styled.div`
+  display: flex;
+  margin: 40px 200px 0px;
+`;
+
+export const MiniContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 33%;
+  margin-top: 35px;
+  margin-left: 20px;
+`;
+
+export const WorkflowImg = styled.img`
+  width: 35px;
+  height: 30px;
+  margin-bottom: 20px;
+`;
+
+export const SubtitleWorkflow = styled.h3`
+  margin-bottom: 20px;
+`;
+
+export const TextWorkflow = styled.p`
+  color: hsl(201, 11%, 66%);
+  text-align: center;
+  margin-left: 50px;
+  margin-right: 50px;
+`;
+
+// icons container
+
+export const IconsContainer = styled.div`
+  margin-top: 75px;
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+export const Icons = styled.img`
+  width: 100px;
+  height: 35px;
+`;
+
+// bottom container
+
+export const BottomContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 75px;
+`;
+
+export const BottomSubtitle = styled.h2`
+  font-size: 2.5em;
+`;
+
+export const BottomText = styled.p`
+  width: 33%;
+  color: hsl(201, 11%, 66%);
+  text-align: center;
+  margin-top: 10px;
+  font-size: 1.2em;
+  white-space: pre-line;
+  line-height: 1.5;
+`;
+
+export const BottomButtons = styled.div`
+  display: flex;
+  margin-top: 30px;
+`;
+
+// footer
+
+export const Footer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 100px;
+  margin-top: 25px;
+`;
+
+// footer logo
+
+export const FooterLogo = styled.img<{ width: string; height: string }>`
+  position: relative;
+  top: 30px;
+  left: 225px;
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+`;
+
+// footer text
+
+export const FooterContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  position: relative;
+  right: 50px;
+  top: 15px;
+  width: 40%;
+  margin-right: 200px;
+  margin-left: 200px;
+`;
+
+export const FooterTextContainer = styled.div``;
+
+export const FooterLinks = styled.a`
+  text-decoration: none;
+  color: hsl(201, 11%, 66%);
+  &:hover {
+    background-color: #0769e9;
+    transition: 0.5s;
+    padding: 4px 6px;
+    border-radius: 5px;
+    color: white;
+    cursor: pointer;
+  }
+`;
+
+// footer icons
+
+export const FooterIconsContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  position: relative;
+  top: 50px;
+  right: 350px;
+  width: 150px;
+`;
+
+export const FooterLink = styled.a``;
+
+export const FooterLinkImg = styled.img`
+  width: 15px;
+  height: 15px;
 `;
